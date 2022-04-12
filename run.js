bedb762266f514234be15af16967122c66a5fe50
Roman S <svroev@gmail.com>
2022-04-12 11:12:58 +0300

    2

--- run.js
+++ run.js
@@ -9,14 +9,23 @@ const server = http.createServer((reqest, response) => {
     {
         let number1 = numbers['number1'];
         let number2 = numbers['number2'];
-        response.statusCode = 200;
-        response.setHeader('name', 'Roman');
-        response.setHeader('number1', number1);
-        response.setHeader('number2', number2);
-        response.setHeader('sum', number1+number2);
-        response.setHeader('minus', number1-number2);
-        response.setHeader('multiply', number1*number2);
-        response.setHeader('div', number1/number2);
+        if(number1 == undefined || number2 == undefined)
+        {
+            response.statusCode = 400;
+        }
+        else
+        {
+           // console.log(numbers);
+            response.setHeader('Access-Control-Allow-Origin', 'origin-list');
+            response.statusCode = 200;
+            response.setHeader('name', 'Roman');
+            response.setHeader('number1', number1);
+            response.setHeader('number2', number2);
+            response.setHeader('sum', Number(number1)+Number(number2));
+            response.setHeader('minus', number1-number2);
+            response.setHeader('multiply', number1*number2);
+            response.setHeader('div', number1/number2);
+        }
     }
     response.write('Hello, World!');
     response.end();
