a84ad2505fd4f3be4594282aa557d21fb3124245
Roman S <svroev@gmail.com>
2022-04-12 10:20:06 +0300

    1.2

--- run.js
+++ run.js
@@ -2,17 +2,17 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((reqest, response) => {
     //console.log(reqest.read());
-    let numers = reqest.headers;
-    let number1 = numbers['number1'];
-    let number2 = numbers['number2'];
-    if(number1 == undefined || number2 == undefined)
+    let numbers = reqest.headers;
+    if(numbers == undefined)
         response.statusCode = 400;
     else
     {
+        let number1 = numbers['number1'];
+        let number2 = numbers['number2'];
         response.statusCode = 200;
         response.setHeader('name', 'Roman');
-        response.setHeader('number1', '3');
-        response.setHeader('number2', '5');
+        response.setHeader('number1', number1);
+        response.setHeader('number2', number2);
         response.setHeader('sum', number1+number2);
         response.setHeader('minus', number1-number2);
         response.setHeader('multiply', number1*number2);
