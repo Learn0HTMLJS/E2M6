38c0c4706e36362620fefd0ebab23a82c2567243
Roman S <svroev@gmail.com>
2022-04-12 11:44:07 +0300

    3

--- run.js
+++ run.js
@@ -18,13 +18,25 @@ const server = http.createServer((reqest, response) => {
            // console.log(numbers);
             response.setHeader('Access-Control-Allow-Origin', 'origin-list');
             response.statusCode = 200;
-            response.setHeader('name', 'Roman');
+            let r =
+            {
+                'name': 'Roman',
+                'number1': number1,
+                'number2': number2,
+                'sum': Number(number1)+Number(number2),
+                'minus': number1-number2,
+                'multiply': number1*number2,
+                'div': number1/number2
+            }
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify(r));
+ /*           response.setHeader('name', 'Roman');
             response.setHeader('number1', number1);
             response.setHeader('number2', number2);
             response.setHeader('sum', Number(number1)+Number(number2));
             response.setHeader('minus', number1-number2);
             response.setHeader('multiply', number1*number2);
-            response.setHeader('div', number1/number2);
+            response.setHeader('div', number1/number2);*/
         }
     }
     response.write('Hello, World!');
